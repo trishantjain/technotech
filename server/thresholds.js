7bb08331950176da3ac257f383fd73a525807ac3
trishantjain <trishantjain4444@gmail.com>
2025-10-06 11:02:58 +0530

    Updated threshold values

--- server/thresholds.js
+++ server/thresholds.js
@@ -1,26 +1,26 @@
 module.exports = {
   insideTemperature: {
     min: -10,
-    max: 50   // Over 50°C triggers fire/alarm
+    max: 75   // Over 50°C triggers fire/alarm
   },
   outsideTemperature: {
-    min: -10,
-    max: 60
+    min: -20,
+    max: 85
   },
   humidity: {
-    min: 20,
-    max: 80   // You can adjust as per your sensor/environment
+    min: 10,
+    max: 95   // You can adjust as per your sensor/environment
   },
   inputVoltage: {
-    min: 3.0,
-    max: 5.0
+    min: 40.0,
+    max: 65.0
   },
   outputVoltage: {
-    min: 3.0,
-    max: 5.0
+    min: 45.0,
+    max: 55.0
   },
   batteryBackup: {
     min: 10,     // minimum 10 mins backup expected
-    max: 120     // assume 2 hrs max for chart normalization
+    max: 14     // assume 2 hrs max for chart normalization
   }
 };
