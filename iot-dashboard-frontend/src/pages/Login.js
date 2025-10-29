29e9ce57db4e9cd59d4975fe8708aa12ea555dcc
trishantjain <trishantjain4444@gmail.com>
2025-10-29 16:17:35 +0530

    Updated Login.js

--- iot-dashboard-frontend/src/pages/Login.js
+++ iot-dashboard-frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
@@ -50,6 +50,7 @@ function Login() {
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          autoFocus
           required
         />
         <input
