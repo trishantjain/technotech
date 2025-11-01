1eaefd6fdb6cf065891d07f4a0b6c88e0c0ccad1
trishantjain <trishantjain4444@gmail.com>
2025-11-01 11:15:09 +0530

    Added API to delete Device

--- server/server.js
+++ server/server.js
@@ -156,6 +156,23 @@ app.put("/api/device/:mac", async (req, res) => {
   }
 });
 
+app.post("/api/device/delete/:mac", async (req, res) => {
+  const { password } = req.body;
+  if (password !== process.env.ADMIN_PASSWORD)
+    return res
+      .status(403)
+      .json({ error: "Unauthorized: Invalid admin password" });
+  try {
+    const result = await Device.deleteOne({ mac: req.params.mac });
+    if (result.deletedCount === 0)
+      return res.status(404).json({ error: "Device not found" });
+    res.json({ message: "Device deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting device:", err);
+    res.status(500).json({ error: "Error deleting device" });
+  }
+});
+
 // ✅ Edit User
 app.put("/api/user/:id", async (req, res) => {
   try {
